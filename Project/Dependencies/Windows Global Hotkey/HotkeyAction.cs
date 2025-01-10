ad410ff08736c8ea8eceedde06a130f3e3d832b4
Olivier Rogier <github.com@ordisoftware.org>
2025-01-10 22:30:39 +0100

    Add file

---
+++ Project/Dependencies/Windows Global Hotkey/HotkeyAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable VSSpell001 // Spell Check
+namespace Base.Hotkeys
+{
+
+  public class HotkeyAction
+  {
+
+    public Hotkey Hotkey { get; set; }
+
+    public Action Execute { get; set; }
+
+    public bool Active { get; set; }
+
+    public HotkeyAction(Hotkey hotkey, Action execute, bool active = true)
+    {
+      Hotkey = hotkey;
+      Execute = execute;
+      Active = active;
+    }
+
+  }
+
+}
+#pragma warning restore VSSpell001 // Spell Check
