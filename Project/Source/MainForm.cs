47cf924b487895a8befc8155f394891f9a6eab6a
Olivier Rogier <github.com@ordisoftware.org>
2025-01-11 21:37:48 +0100

    Update file

--- Project/Source/MainForm.cs
+++ Project/Source/MainForm.cs
@@ -1,4 +1,18 @@
-﻿using SQLite;
+﻿/// <license>
+/// This file is part of Ordisoftware Hebrew Pi.
+/// Copyright 2025 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file,
+/// then You may include the notice in a location(such as a LICENSE file in a
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2025-01-10 </created>
+/// <edited> 2025-01-11 </edited>
+using SQLite;
 
 namespace Ordisoftware.Hebrew.Pi;
 
@@ -34,6 +48,23 @@ public partial class MainForm : Form
   }
 
   PiDecimalsExtractSize FileName = PiDecimalsExtractSize.PiDecimals_10B;
+
+  #region Singleton
+
+  /// <summary>
+  /// Indicates the singleton instance.
+  /// </summary>
+  static internal MainForm Instance { get; private set; }
+
+  /// <summary>
+  /// Static constructor.
+  /// </summary>
+  static MainForm()
+  {
+    Instance = new MainForm();
+  }
+
+  #endregion
 
   public MainForm()
   {
